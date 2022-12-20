3868001963bf2b12e13655190ffe0c1c5f57b863
Enderson Rufino Amorim <endersonrufino@gmail.com>
2022-12-20 00:10:18 -0300

    initial commit

---
+++ AppWeb.ClientMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppWeb.ClientMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
